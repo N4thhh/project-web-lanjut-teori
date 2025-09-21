ebb493e381573c4f103ccc072d023598b5be1db3
N4thhh <natanaelbungaran@gmail.com>
2025-09-21 13:28:08 +0700

    Migrate Database

--- database/migrations/2025_09_21_061305_create_order_details_table.php
+++ database/migrations/2025_09_21_061305_create_order_details_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('order_details', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignId('order_id')->constrained('orders')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('laundry_services');
+            $table->integer('jumlah');
+            $table->decimal('subtotal', 12, 2);
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('order_details');
     }
 };
